cc6d3eb3be34c71577bf3e035e49607a82a466da
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-06 01:18:38 +0300

    Добавлены комментарии в в класс BonusMilesService

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
         public int calculate(int price, int cost){
 
-            int miles = price/cost;
+            int miles = price/cost; // price - цена билета, cost - стоимость одной мили
             return miles;
 
         }
