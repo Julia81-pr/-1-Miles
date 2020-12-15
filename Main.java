f7cd343890ab1d815f4217b428f67ee1b3383195
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-15 21:51:43 +0300

    Изменены:
    Названия переменных
    Способ подсчета переменных

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите стоимость билета:");
         int priceTicket = sc.nextInt();
-        int miles = service.calculate(priceTicket,20);
-        System.out.println("Полученные мили: " + miles);
+        int costOneMile = 20;
+        int gotMiles = priceTicket/ costOneMile;
+        System.out.println("Полученные мили: " + gotMiles);
     }
 }
-
-//
